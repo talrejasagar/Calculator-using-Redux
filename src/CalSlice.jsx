c88d583ef19a9f923abbee4e3f27b96087c6b0ea
Sagar Talreja <talreja.sagar97@gmail.com>
2022-05-27 19:52:23 +0530

    Updated with all functionalities

--- src/CalSlice.jsx
+++ src/CalSlice.jsx
@@ -7,11 +7,22 @@ const CalOperation = createSlice({
   },
   reducers: {
     add: (state, action) => {
-      console.log(action);
       state.result = +action.payload.input1 + +action.payload.input2;
+    },
+    substract: (state, action) => {
+      state.result = action.payload.input1 - action.payload.input2;
+    },
+    multiply: (state, action) => {
+      state.result = action.payload.input1 * action.payload.input2;
+    },
+    divide: (state, action) => {
+      state.result = action.payload.input1 / action.payload.input2;
+    },
+    clear: (state) => {
+      state.result = "";
     },
   },
 });
-export const { add } = CalOperation.actions;
+export const { add, substract, multiply, divide, clear } = CalOperation.actions;
 
 export default CalOperation.reducer;
--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { add } from "./CalSlice";
+import { add, multiply, substract, divide, clear } from "./CalSlice";
 
 const Calculator = () => {
   const [input1, SetInput1] = useState("");
@@ -9,21 +9,40 @@ const Calculator = () => {
   const result = useSelector((state) => {
     return state.CalReducer.result;
   });
+  const clearState = () => {
+    SetInput1("");
+    SetInput2("");
+    dispatch(clear());
+  };
 
   return (
     <>
       <input
         type="number"
         value={input1}
+        placeholder="Please enter Number"
         onChange={(e) => SetInput1(e.target.value)}
       />
+      <br />
       <input
         type="number"
         value={input2}
+        placeholder="Please enter Number"
         onChange={(e) => SetInput2(e.target.value)}
       />
+      <br />
+      <h1>{result}</h1>
       <button onClick={() => dispatch(add({ input1, input2 }))}>Add</button>
-      {result}
+      <button onClick={() => dispatch(substract({ input1, input2 }))}>
+        Substract
+      </button>
+      <button onClick={() => dispatch(multiply({ input1, input2 }))}>
+        Multiply
+      </button>
+      <button onClick={() => dispatch(divide({ input1, input2 }))}>
+        Divide
+      </button>
+      <button onClick={clearState}>Clear</button>
     </>
   );
 };
